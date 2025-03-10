f78bad9fb0e8ead620e0a2fc63afe5f79b655b44
Harshithrao1224 <127205292+Harshithrao1224@users.noreply.github.com>
2025-03-10 12:42:10 +0530

    Harshith Commit

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,10 @@
+import "../styles/Footer.scss"
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h1> This is the Footer Component...</h1>
+    </div>
+  )
+}
+
+export default Footer
