f795d81a6f61d6614b28220327cec4a7ef39bc2d
Damodhar <damodhar@divami.com>
2025-03-10 15:16:34 +0530

    Displaying the userData

--- src/Components/Display.tsx
+++ src/Components/Display.tsx
@@ -1,24 +1,28 @@
-import React, { useState } from 'react'
-import styles from 'display.module.scss'
-const Display = ({items}) => {
-    const [data, setData] = useState<object[]>(items);
-  return (
-   <>
-    <div className="displayUsers">
-        {
-            data.map((val)=>
-            (
-                <div className={styles.cards}>
-                    <h3>Name of the User : {val?.name}</h3>
-                    <h3>Email : {val?.email}</h3>
-                    <h3>Date of Birth : {val?.DOB}</h3>
-                    <h3>Mobile Number : {val?.Mobile}</h3>
-                </div>
-            ))
-        }
-    </div>
-   </>
-  )
-}
+import React, { useEffect, useState } from 'react'
+import styles from './display.module.scss'
+const Display = ({ array }: any) => {
 
+    localStorage.setItem('UserData', JSON.stringify(array));
+    console.log("array", array);
+    // console.log("props",props);
+    const data: object[] = JSON.parse(localStorage.getItem('UserData') || '{}');
+    console.log("data", data);
+    return (
+        <>
+            <div className="displayUsers">
+                {
+                    data.map((val: any) =>
+                    (
+                        <div className={styles.cards}>
+                            <h3>Name of the User : {val?.name}</h3>
+                            <h3>Email : {val?.email}</h3>
+                            <h3>Date of Birth : {val?.DOB}</h3>
+                            <h3>Mobile Number : {val?.Mobile}</h3>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    )
+}
 export default Display
