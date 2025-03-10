7351079b166a272cb68017c629eaede4a93848ff
Damodhar <damodhar@divami.com>
2025-03-10 12:56:00 +0530

    Working on Displaying the user Details

--- src/Components/Display.tsx
+++ src/Components/Display.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
-
-const Display = () => {
+import React, { useState } from 'react'
+import styles from 'display.module.scss'
+const Display = ({items}) => {
+    const [data, setData] = useState<object[]>(items);
   return (
-    <div>Display</div>
+   <>
+    <div className="displayUsers">
+        {
+            data.map((val)=>
+            (
+                <div className={styles.cards}>
+                    <h3>Name of the User : {val?.name}</h3>
+                    <h3>Email : {val?.email}</h3>
+                    <h3>Date of Birth : {val?.DOB}</h3>
+                    <h3>Mobile Number : {val?.Mobile}</h3>
+                </div>
+            ))
+        }
+    </div>
+   </>
   )
 }
 
